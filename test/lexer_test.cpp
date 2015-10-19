69caea9ac0ea933cc99e55f043b841b7506079ea
Jan-Alexander Mock <janalexander.mock@gmail.com>
2015-10-19 19:35:40 +0200

    Clang crash

--- test/lexer_test.cpp
+++ test/lexer_test.cpp
@@ -1,8 +1,34 @@
 #include "catch.hpp"
 #include "qac/lexer/lexer.h"
 
-TEST_CASE( "lexer test", "[lexer]" ) {
-    SECTION( "test 1" ) {
-        REQUIRE(true);
+#include <sstream>
+#include <vector>
+
+using namespace qac;
+
+TEST_CASE("lexer test", "[lexer]") {
+    SECTION("hello world lexer text") {
+        std::stringstream ss;
+
+        ss << "Q: Hello\n"
+           << "A: World\n";
+
+        std::vector<token> expected = {
+            token(token_enum::QUESTION, "Q:", 1),
+            token(token_enum::WORD, "Hello", 1),
+            token(token_enum::NEW_LINE, "", 1),
+
+            token(token_enum::ANSWER, "A:", 2),
+            token(token_enum::WORD, "World", 2),
+            token(token_enum::NEW_LINE, "", 2),
+        };
+
+        lexer l;
+        std::vector<token> actual = l.lex(ss);
+
+        token a(token_enum::ANSWER, "A:", 3);
+        token b(token_enum::ANSWER, "A:", 2);
+
+        REQUIRE(a == b);
     }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+template <>
+std::string Catch::toString<qac::token>(token const& value) {
+    return "token";
+}
