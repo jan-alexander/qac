2e600c0433d47e3cdd472e3f7ff32eb18b2f3666
Jan-Alexander Mock <janalexander.mock@gmail.com>
2015-10-19 08:52:44 +0200

    Started to implement tests

---
+++ test/lexer_test.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+#include "qac/lexer/lexer.h"
+
+TEST_CASE( "lexer test", "[lexer]" ) {
+    SECTION( "test 1" ) {
+        REQUIRE(true);
+    }
+}
---
+++ test/parser_test.cpp
@@ -0,0 +1,9 @@
+#include "catch.hpp"
+#include "qac/parser/parser.h"
+
+TEST_CASE( "parser test", "[parser]" ) {
+    SECTION( "test 1" ) {
+        REQUIRE(true);
+    }
+}
+
